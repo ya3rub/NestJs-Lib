040669f611ac0b0d3d41cbe4f14a3bf4388950a7
AhmedSalah101 <ahmed.salah.y101@gmail.com>
2022-10-14 01:10:19 +0200

    minor changes

--- src/email-confirmation/guards/emailConfirmation.guard.ts
+++ src/email-confirmation/guards/emailConfirmation.guard.ts
@@ -8,18 +8,6 @@ import {
 import { JwtAuthGuard } from 'src/auth/guards';
 import { RequestWithUser } from 'src/auth/interfaces';
 
-// @Injectable()
-// export class EmailConfirmationGuard implements CanActivate {
-//   canActivate(context: ExecutionContext) {
-//     const request: RequestWithUser = context.switchToHttp().getRequest();
-
-//     if (!request.user?.isEmailConfirmed) {
-//       throw new UnauthorizedException('Confirm your email first');
-//     }
-
-//     return true;
-//   }
-// }
 @Injectable()
 export class EmailConfirmationGuard extends JwtAuthGuard {
   async canActivate(context: ExecutionContext) {
